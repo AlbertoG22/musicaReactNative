4ceaf623205b9ae6620b4d19b40dd0eb396c1726
5j <5j@iMac-de-facultad.local>
2019-11-13 08:40:47 -0600

    first commit

---
+++ MiMusica/App.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import LoginView from './src/LoginView';
+import HomeView from './src/HomeView';
+import {Actions, Scene, Router } from 'react-native-router-flux';
+
+const scenes = Actions.create(
+  <Scene key ="root">
+    <Scene key="login" component={LoginView} hideNavBar/>
+    <Scene key="home" component={HomeView} hideNavBar/>
+  </Scene>
+)
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return <Router scenes={scenes}/>
+  }
+}
+
+/*const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});*/
